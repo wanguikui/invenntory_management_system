071e76940d313b63c4d61a51b986ff26752c4529
wanguikui <wangui.munyua@gmail.com>
2020-04-23 16:44:56 -0700

    Fix

--- Config/Config.py
+++ Config/Config.py
@@ -9,7 +9,7 @@ class Development(Config):
     SQLALCHEMY_DATABASE_URI='postgresql://postgres:wagatagati12!@127.0.0.1:5432/inventory_management_system'
 
 
-class Production(Config):
+class Production():
     SQLALCHEMY_DATABASE_URI='postgres://gilkahqjevhnwe:16cf303eb194d57d2ed6514153ce474dca171a7914266a5b09c4df9d520e70ed@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d6sa56nv73mkjh'
     SECRET_KEY='mnmnmnmnmnmnmnmnmnmnm12'
 
